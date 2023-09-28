2d945c5346d33f838c14ccaa9d2d9aefe554a96c
OkFrank12 <cfoonyemmemme@gmail.com>
2023-09-28 22:16:28 +0100

    franklin's read

--- router/profilerouter.ts
+++ router/profilerouter.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+  createProfile,
+  daleteProfile,
+  updateCompanyProfile,
+  updateProfileInfo,
+  viewProfile,
+} from "../controller/profilecontroller";
 
+const router = express.Router();
 
-const router = express.Router()
+router.route("/create-profile").post(createProfile);
+router.route("/view-profile").get(viewProfile);
+router.route("/:profileID/update-company-profile").patch(updateCompanyProfile);
+router.route("/:profileID/update-company-info").patch(updateProfileInfo);
+router.route("/:profileID/delete-profile").delete(daleteProfile);
 
+export default router;
