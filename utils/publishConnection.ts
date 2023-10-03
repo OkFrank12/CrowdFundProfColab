2d9100e06ad8a40af19d0571718c613848f0a83b
OkFrank12 <cfoonyemmemme@gmail.com>
2023-10-03 13:40:59 +0100

    excel's deployment

--- utils/publishConnection.ts
+++ utils/publishConnection.ts
@@ -1,7 +1,7 @@
 import amqplib from "amqplib";
 
-const amqplibServer =
-  "amqps://ytczyrcc:VVy6y7RE1kt3-FCMs_kV1621467bNh0t@whale.rmq.cloudamqp.com/ytczyrcc";
+// const amqplibServer =
+//   "amqps://ytczyrcc:VVy6y7RE1kt3-FCMs_kV1621467bNh0t@whale.rmq.cloudamqp.com/ytczyrcc";
 const amqplibServer = "amqps://ytczyrcc:VVy6y7RE1kt3-FCMs_kV1621467bNh0t@whale.rmq.cloudamqp.com/ytczyrcc";
 
 export const publishConnection = async (queue: string, data: any) => {
